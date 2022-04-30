2ff30bb9abd2eaf5c2bca1a9c11ee143473251fb
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2022-04-30 23:49:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import { AddEvent } from './components/AddEvent';
+import Cards from './components/Card';
 
 function App() {
   return (
-    <div className="container mt-3" >
-      <div className="col-md-5">
-      <fieldset>
-            <AddEvent />
-      </fieldset>
+    <div>
+      <div className='view'>
+        <div className="container mt-5" >
+          <div className="desc">
+            <h1>Input your new event here</h1>
+          </div>
+          <div className="col-md-5">
+            <div className="test">
+              <AddEvent />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='card'>
+        <h1>Event List</h1>
       </div>
     </div>
   );
